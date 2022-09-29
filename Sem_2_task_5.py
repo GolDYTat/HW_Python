8c5deda96c88fc437676f9c892094e7e2ffbb0a4
Danil <31danet@gmail.com>
2022-09-29 19:37:28 +0300

    added Sem_2_task_5

--- Sem_2_task_5.py
+++ Sem_2_task_5.py
@@ -4,7 +4,14 @@ from random import randint
 len = int(input('Введите число: '))
 a = []
 for i in range(len):
-    a.append(randint(-len, len))
+    i = randint(-len, len)
+    a.append(i)
 print('Исходный список =>', a)
 
-f = open(file.txt)
+f = open('file.txt')
+b = []
+for i in f:
+    b.append(i)
+
+mult = a[int(b[0])] * a[int(b[1])]
+print(f'Произведение элементов с индексами, которые хранятся в file.txt равняется: {mult}')
